dcd681b3fd3170b1924b5f03c4c972c200e21c70
fferradamerino <156674516+fferradamerino@users.noreply.github.com>
2025-11-17 11:07:09 -0300

    .

--- precertamen/src/App.js
+++ precertamen/src/App.js
@@ -2,7 +2,6 @@ import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import { Partidas } from "./components/Partidas"; //cambiar a componente principal
 
-
 export function App() {
   return (
     <Fragment>
